cd037144d65ac6d723c2621a9d33858239c27463
Gavin King <gavin.king@itslearning.com>
2017-07-16 15:22:05 +0200

    Fiddling with serverport configurations

--- src/client.js
+++ src/client.js
@@ -2,6 +2,10 @@
 
 const config = require('config');
 
+const serverProtocol = config.get('server.protocol') || 'http';
+const serverHost     = config.get('server.host') || 'localhost';
+let serverPort       = config.get('server.port') || 3000;
+
 const Log = require('./log');
 const log = new Log({
     'level': config.get('log.level'),
@@ -21,17 +25,16 @@ const argv = require('yargs')
     .usage('Usage: $0 [args]')
     .option('sp', {
         'alias': 'server-port',
-        'default': 3000,
+        'default': serverPort,
         'describe': 'The socket server port to connect to'
     })
     .help()
     .argv;
 
-const serverProtocol = config.get('server.protocol') || 'http';
-const serverHost     = config.get('server.host') || 'localhost';
-const serverPort     = argv.serverPort || config.get('server.port');
+// we may want to overwrite the configs and default on the CLI
+serverPort = argv.serverPort || serverPort;
 
-const serverPath     = `${serverProtocol}://${serverHost}:${serverPort}`;
+const serverPath = `${serverProtocol}://${serverHost}:${serverPort}`;
 
 log.info(`trying to connect to ${serverPath}`);
 
